113c78424d3019500015080db72cf8e111793820
eashwarbalu <eashwareanish1000@gmail.com>
2026-02-06 22:32:46 +0530

    completed project

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = () => {
+    if (task.trim() === "") return;
+
+    setTasks([...tasks, { text: task, completed: false }]);
+    setTask("");
+  };
+
+  const toggleTask = (index) => {
+    const newTasks = [...tasks];
+    newTasks[index].completed = !newTasks[index].completed;
+    setTasks(newTasks);
+  };
+
+
+  const deleteTask = (index) => {
+    const newTasks = tasks.filter((_, i) => i !== index);
+    setTasks(newTasks);
+  };
+
+  return (
+    <div className="container">
+      <h1>To-Do List</h1>
+
+      <div className="input-box">
+        <input
+          type="text"
+          placeholder="Enter a task..."
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+        />
+        <button onClick={addTask}>Add</button>
+      </div>
+
+      <ul>
+        {tasks.map((t, index) => (
+          <li key={index} className={t.completed ? "done" : ""}>
+            <span>{t.text}</span>
+
+            <div className="buttons">
+              <button onClick={() => toggleTask(index)}>
+                {t.completed ? "Undo" : "Complete"}
+              </button>
+              <button onClick={() => deleteTask(index)}>Delete</button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
